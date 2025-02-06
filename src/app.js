6e93b2ec3b6c80614a785c1e070d3d2d71cbc01f
jrag-dev <josealvarado2117@gmail.com>
2025-02-06 00:37:18 -0400

    refactor(api) simplify app and server files

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+
+class App {
+
+  constructor(){
+    this.app = express();
+  }
+
+  middlewares() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(cors());
+    this.app.use(helmet());
+  }
+
+  routes() {
+    this.app.get('/', (req, res) => {
+      res.send("This is my products manager api");
+    })
+  }
+
+}
+
+export default new App().app;
